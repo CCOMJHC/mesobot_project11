973111b40223289dab9da8b3b9e969ff13b28844
Field User <field@ccom.unh.edu>
2021-11-02 18:22:35 +0000

    Augment position with status info

--- nodes/mesobot_node.py
+++ nodes/mesobot_node.py
@@ -8,10 +8,15 @@ from geographic_msgs.msg import GeoPoseStamped
 from marine_msgs.msg import Heartbeat
 from marine_msgs.msg import KeyValue as HBKeyValue
 
+import math
+
+import project11
+from tf.transformations import quaternion_about_axis
 
 last_state = None
 
 def smsCallback( msg):
+  global last_state
   print (msg)
 
   decoder = {'H:':'Heading',
@@ -28,8 +33,10 @@ def smsCallback( msg):
     print (parts)
     key = None
     values = []
+
+    state = {'timestamp':rospy.Time.now()}
     hb = Heartbeat()
-    hb.header.stamp = rospy.Time.now()
+    hb.header.stamp = state['timestamp']
     for p in parts:
       if p in decoder.keys():
         if key is not None:
@@ -41,18 +48,38 @@ def smsCallback( msg):
         values = []
       else:
         values.append(p)
+        if key == 'H:' and not 'heading' in state:
+          state['heading'] = float(p)
+        if key == 'D:' and not 'depth' in state:
+          state['depth'] = float(p)
+
     if key is not None:
       kv = HBKeyValue()
       kv.key = decoder[key]
       kv.value = ', '.join(values)
       hb.values.append(kv)
     heartbeat_pub.publish(hb)
+    last_state = state
     
 
 
 
 def positionCallback(msg):
-  pass
+  gps = GeoPoseStamped()
+  gps.header = msg.header
+  gps.pose.position = msg.position
+  if last_state is not None:
+    if 'timestamp' in last_state and rospy.Time().now() - last_state['timestamp'] < rospy.Duration(secs=60):
+      if 'depth' in last_state:
+        gps.pose.position.altitude = -last_state['depth']
+      if 'heading' in last_state:
+        yaw = math.radians(project11.nav.headingToYaw(last_state['heading']))
+        quat = quaternion_about_axis(yaw, (0,0,1))
+        gps.pose.orientation.x = quat[0]
+        gps.pose.orientation.y = quat[1]
+        gps.pose.orientation.z = quat[2]
+        gps.pose.orientation.w = quat[3]
+  position_pub.publish(gps)
   #print (msg)
 
 rospy.init_node('command_bridge_sender', anonymous=False)
