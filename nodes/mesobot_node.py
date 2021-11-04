c88c9c7238947fef2d2693133ffed8209cdfd158
Field User <field@ccom.unh.edu>
2021-11-04 18:43:26 +0000

    Add backup position input

--- nodes/mesobot_node.py
+++ nodes/mesobot_node.py
@@ -15,6 +15,8 @@ from tf.transformations import quaternion_about_axis
 
 last_state = None
 
+last_positon_time = None
+
 def smsCallback( msg):
   global last_state
   print (msg)
@@ -25,7 +27,8 @@ def smsCallback( msg):
              'R:':'Radiometer',
              'S:':'Flows',
              'C:':'Command',
-             'M:':'Status'
+             'M:':'Status',
+             'J:':'Drive'
              }
 
   if msg.message.startswith('H: '):
@@ -69,7 +72,7 @@ def positionCallback(msg):
   gps.header = msg.header
   gps.pose.position = msg.position
   if last_state is not None:
-    if 'timestamp' in last_state and rospy.Time().now() - last_state['timestamp'] < rospy.Duration(secs=60):
+    if 'timestamp' in last_state and rospy.Time().now() - last_state['timestamp'] < rospy.Duration(secs=300):
       if 'depth' in last_state:
         gps.pose.position.altitude = -last_state['depth']
       if 'heading' in last_state:
@@ -80,12 +83,34 @@ def positionCallback(msg):
         gps.pose.orientation.z = quat[2]
         gps.pose.orientation.w = quat[3]
   position_pub.publish(gps)
+  last_positon_time = gps.header.stamp
   #print (msg)
+
+def backupPositionCallback(msg):
+  gps = GeoPoseStamped()
+  gps.header = msg.header
+  gps.pose.position = msg.position
+  if last_state is not None:
+    if 'timestamp' in last_state and rospy.Time().now() - last_state['timestamp'] < rospy.Duration(secs=300):
+      if 'depth' in last_state:
+        gps.pose.position.altitude = -last_state['depth']
+      if 'heading' in last_state:
+        yaw = math.radians(project11.nav.headingToYaw(last_state['heading']))
+        quat = quaternion_about_axis(yaw, (0,0,1))
+        gps.pose.orientation.x = quat[0]
+        gps.pose.orientation.y = quat[1]
+        gps.pose.orientation.z = quat[2]
+        gps.pose.orientation.w = quat[3]
+  if last_positon_time is None or msg.header.stamp - last_positon_time > rospy.Duration(30):
+    position_pub.publish(gps)
+  #print (msg)
+
 
 rospy.init_node('command_bridge_sender', anonymous=False)
 
 sms_sub = rospy.Subscriber('sms', SMS, smsCallback)
 position_sub = rospy.Subscriber('position', GeoPointStamped, positionCallback)
+backup_position_sub = rospy.Subscriber('backup_position', GeoPointStamped, backupPositionCallback)
 
 heartbeat_pub = rospy.Publisher('project11/heartbeat', Heartbeat, queue_size=1)
 position_pub = rospy.Publisher('nav/position', GeoPoseStamped, queue_size=1)
